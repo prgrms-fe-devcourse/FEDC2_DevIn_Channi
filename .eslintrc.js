97a1f0e0f5b665925123b192db65792a4177fccf
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-06-08 18:29:15 +0900

    feat: eslint-prettier airbnb 스타일 가이드로 추가

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['react-hooks'],
+  env: {
+    browser: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
