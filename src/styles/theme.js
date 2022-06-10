d10afa9b92176b4edcf56ded7b406da3bef6c811
lexie-kaia <kaia.gayeon.kim@gmail.com>
2022-06-10 21:48:41 +0900

    feat: theme 변경

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,11 @@
 export const theme = {
   color: {
     highlight: '#00bbff',
-    darlBlue: '#008bcc',
+    blue: '#1D9BF0',
     green: '#15d490',
-    red: '#f96274',
-    blackMain: '#ffffff',
-    blackSub: '#f2f2f2',
+    red: '#ff6c6c',
+    backMain: '#ffffff',
+    backSub: '#f2f2f2',
     line: '#e6e6e6',
     textPrime: '#191919',
     textSecond: '#757575',
