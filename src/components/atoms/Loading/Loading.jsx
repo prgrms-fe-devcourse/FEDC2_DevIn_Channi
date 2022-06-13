b89ac80066ebc9ddfe39226c57621d3536bfed5a
parkinhwa <inhwa6548@naver.com>
2022-06-13 23:43:24 +0900

    feat: Loading Spinner 추가

---
+++ src/components/atoms/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import { Icon } from 'components';
+import * as S from './style';
+
+export function Loading() {
+  return (
+    <S.Loading>
+      <Icon icon="spinner"/>
+    </S.Loading>
+  );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export { Icon } from 'components/atoms/Icon/Icon';
 export { ProfileUserImage } from 'components/atoms/ProfileUserImage/ProfileUserImage';
 export { ProfileUserName } from 'components/atoms/ProfileUserName/ProfileUserName';
 export { FollowButton } from 'components/atoms/FollowButton/FollowButton';
+export { Loading } from 'components/atoms/Loading/Loading';
 
 export { Profile } from 'components/molecules/Profile/Profile';
 
