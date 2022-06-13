bdae4be8922c745efa4048ab2bcc184862a7c15b
parkinhwa <inhwa6548@naver.com>
2022-06-13 15:15:58 +0900

    feat: 컴포넌트 export 설정

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
-// atoms
 export { Icon } from 'components/atoms/Icon/Icon';
+export { ProfileUserImage } from 'components/atoms/ProfileUserImage/ProfileUserImage';
+export { ProfileUserName } from 'components/atoms/ProfileUserName/ProfileUserName';
+export { FollowButton } from 'components/atoms/FollowButton/FollowButton';
+
+export { Profile } from 'components/molecules/Profile/Profile';
+
+export { ProfileList } from 'components/organisms/ProfileList/ProfileList';
