cf35788089be89d08b281b8e4a4331fd82ace692
parkinhwa <inhwa6548@naver.com>
2022-06-14 15:24:25 +0900

    feat: Search Icon 추가

--- src/components/atoms/SearchIcon/SearchIcon.jsx
+++ src/components/atoms/SearchIcon/SearchIcon.jsx
@@ -1,5 +1,7 @@
 import * as S from './style'; 
 
 export function SearchIcon() {
-    return <S.SearchIcon/>
+    return <S.SearchIcon>
+        <S.CustomIcon icon="search"/>
+    </S.SearchIcon>
 }
