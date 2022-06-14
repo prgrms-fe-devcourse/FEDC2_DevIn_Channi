ba703de6cba9f7ee7e304ddae012ad0586a1c66b
parkinhwa <inhwa6548@naver.com>
2022-06-14 17:03:14 +0900

    feat: users 불러올 때 Loading 1초 추가

--- src/api/users.js
+++ src/api/users.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
+function wait(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
 export const users = {
-  getUsers: async ({offset, limit}) => {
+  getUsers: async ({ offset, limit }) => {
     try {
-      const response = await axios.get('/users/get-users', {params: {offset, limit}});
+      const response = await axios.get('/users/get-users', {
+        params: { offset, limit },
+      });
+      await wait(1000);
       return response.data;
     } catch (e) {
       console.error(e);
