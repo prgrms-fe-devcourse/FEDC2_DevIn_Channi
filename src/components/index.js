77a37b171b1f4d3365ec75f15461571c0b28c6a1
parkinhwa <inhwa6548@naver.com>
2022-06-14 14:57:35 +0900

    feat: Search 컴포넌트 export 추가

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { SearchIcon } from 'components/atoms/SearchIcon/SearchIcon';
+export { SearchInput } from 'components/atoms/SearchInput/SearchInput';
+export { SearchNone } from 'components/atoms/SearchNone/SearchNone';
+export { SearchNumber } from 'components/atoms/SearchNumber/SearchNumber';
+export { SearchTarget } from 'components/atoms/SearchTarget/SearchTarget';
+
+export { SearchBar } from 'components/molecules/SearchBar/SearchBar';
+export { SearchResultItem } from 'components/molecules/SearchResultItem/SearchResultItem';
+
+export { SearchResultTab } from 'components/organisms/SearchResultTab/SearchResultTab';
