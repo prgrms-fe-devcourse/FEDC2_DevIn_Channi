b0f57e90a884c40656dfd1a9fcd7d7f7962c2cb4
parkinhwa <inhwa6548@naver.com>
2022-06-14 18:55:22 +0900

    feat: SearchInput text 추가

--- src/components/atoms/SearchInput/SearchInput.jsx
+++ src/components/atoms/SearchInput/SearchInput.jsx
@@ -1,5 +1,11 @@
+import { useState } from 'react';
 import * as S from './style';
 
 export function SearchInput() {
-  return <S.SearchInput/>
+  const [text, setText] = useState('');
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+  console.log(text);
+  return <S.SearchInput type="text" value={text} onChange={onChange}/>;
 }
