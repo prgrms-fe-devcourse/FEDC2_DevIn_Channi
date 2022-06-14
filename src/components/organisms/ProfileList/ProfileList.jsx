a9d84c12d317c727214d91cafd59c256385ac3cf
parkinhwa <inhwa6548@naver.com>
2022-06-14 17:05:52 +0900

    refactory: useEffect에 fetch 한번에 선언

--- src/components/organisms/ProfileList/ProfileList.jsx
+++ src/components/organisms/ProfileList/ProfileList.jsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
 import { Profile, Loading } from 'components';
 import { users } from 'api';
 import * as S from './style';
@@ -7,41 +8,39 @@ import * as S from './style';
 // 검색 결과는 offset, limit 값 지정 못 함
 export function ProfileList() {
   const [loading, setLoading] = useState(false);
-  const [User, setUser] = useState([]);
+  const [user, setUser] = useState([]);
+  const [isUserExsist, setIsUserExsist] = useState(true);
   const [count, setCount] = useState(10);
 
   const getCount = setcount => {
     setCount(setcount + 10);
   };
 
-  const getUsers = useCallback(async () => {
-    setLoading(true);
-    const getuser = await users.getUsers({ offset: 0, limit: count });
-    setUser(getuser);
-    setLoading(false);
+  useEffect(() => {
+    const getUsers = async () => {
+      setLoading(true);
+      const usersInfo = await users.getUsers({ offset: 0, limit: count });
+      setUser(usersInfo);
+      setIsUserExsist(usersInfo.length > 0);
+      setLoading(false);
+    }
+    getUsers();
   }, [count]);
 
-  useEffect(() => {
-      getUsers();
-  }, [getUsers]);
-
-  const profileList =
-    !loading && User.length === 0 ? (
-      // SearchNone 컴포넌트 연결 예정
-      <div>검색결과없음</div>
-    ) : (
-      User.map((user, idx) => (
-        <Profile
-          getCount={getCount}
-          idx={idx + 1}
-          key={user.id}
-          userImage={user.image}
-          userName={user.fullName}
-        />
-      ))
-    );
-  return <S.ProfileList>
-    {profileList}
-    {loading ? <Loading/> : null}
-  </S.ProfileList>;
+  return (
+    <S.ProfileList>
+      {!isUserExsist && <div>검색 결과가 없습니다.</div>}
+      {isUserExsist &&
+        user.map((userInfo, idx) => (
+          <Profile
+            getCount={getCount}
+            idx={idx + 1}
+            key={nanoid()}
+            userImage={userInfo.image || ''}
+            userName={userInfo.fullName}
+          />
+        ))}
+      {loading && <Loading />}
+    </S.ProfileList>
+  );
 }
