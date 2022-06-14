605d354f8ba3d6fea886a09b63d38cd65fc93f0d
parkinhwa <inhwa6548@naver.com>
2022-06-14 18:54:59 +0900

    feat: search api 추가

--- src/api/search.js
+++ src/api/search.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const users = {
-  getUsers: async () => {
+export const search = {
+  search: async text => {
     try {
-      const response = await axios.get('/search/all', {});
+      const response = await axios.get(`/search/all/${text}`);
       return response.data;
     } catch (e) {
       console.error(e);
