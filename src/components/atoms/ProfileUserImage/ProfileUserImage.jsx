d4cdce8643e5dcbf31de8a1d010c51f5d19dede8
parkinhwa <inhwa6548@naver.com>
2022-06-14 17:04:33 +0900

    feat: userImage propType 검사 추가

--- src/components/atoms/ProfileUserImage/ProfileUserImage.jsx
+++ src/components/atoms/ProfileUserImage/ProfileUserImage.jsx
@@ -1,10 +1,15 @@
-import defaultImg from "assets/images/default-profile.jpg";
+import PropTypes from 'prop-types';
+import defaultImg from 'assets/images/default-profile.jpg';
 import * as S from './style';
 
-export function ProfileUserImage(user) {
-  let { userImage } = user;
-  if(!userImage) userImage = defaultImg
-  return (
-    <S.UserImage alt="" src={userImage}/>
-  );
+export function ProfileUserImage({ userImage }) {
+  return <S.UserImage alt="" src={userImage || defaultImg} />;
 }
+
+ProfileUserImage.propTypes = {
+  userImage: PropTypes.string,
+};
+
+ProfileUserImage.defaultProps = {
+  userImage: null,
+};
