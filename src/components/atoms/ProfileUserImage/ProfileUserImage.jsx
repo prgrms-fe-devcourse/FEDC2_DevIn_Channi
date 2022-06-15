bb37348b99338eda20928070a491db1ac02ac859
parkinhwa <inhwa6548@naver.com>
2022-06-15 23:43:01 +0900

    fix: ProfileUserImage 사이즈 prop으로 받도록 변경

--- src/components/atoms/ProfileUserImage/ProfileUserImage.jsx
+++ src/components/atoms/ProfileUserImage/ProfileUserImage.jsx
@@ -2,12 +2,13 @@ import PropTypes from 'prop-types';
 import defaultImg from 'assets/images/default-profile.jpg';
 import * as S from './style';
 
-export function ProfileUserImage({ userImage }) {
-  return <S.UserImage alt="" src={userImage || defaultImg} />;
+export function ProfileUserImage({ userImage, size }) {
+  return <S.UserImage alt="" src={userImage || defaultImg} size={size} />;
 }
 
 ProfileUserImage.propTypes = {
   userImage: PropTypes.string,
+  size: PropTypes.number.isRequired,
 };
 
 ProfileUserImage.defaultProps = {
