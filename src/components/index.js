68a97065cef4c183592f913bb1f7d786878dadda
lexie-kaia <kaia.gayeon.kim@gmail.com>
2022-06-17 18:43:01 +0900

    feat: components index.js 수정

--- src/components/index.js
+++ src/components/index.js
@@ -16,6 +16,24 @@ export { Logo } from 'components/atoms/Logo/Logo';
 export { Divider } from 'components/atoms/Divider/Divider';
 export { SamplePost } from 'components/atoms/SamplePost/SamplePost';
 
+// interactives
+export { WrapperLink } from 'components/atoms-temp/Interactives/WrapperLink/WrapperLink';
+export { TextLink } from 'components/atoms-temp/Interactives/TextLink/TextLink';
+export { IconBtn } from 'components/atoms-temp/Interactives/IconBtn/IconBtn';
+export { TextBtn } from 'components/atoms-temp/Interactives/TextBtn/TextBtn';
+export { RoundInput } from 'components/atoms-temp/Interactives/RoundInput/RoundInput';
+
+// text
+export { Span } from 'components/atoms-temp/Text/Span/Span';
+export { Paragraph } from 'components/atoms-temp/Text/Paragraph/Paragraph';
+export { Time } from 'components/atoms-temp/Text/Time/Time';
+
+// multimedia
+export { Avatar } from 'components/atoms-temp/multimedia/Avatar/Avatar';
+export { Image } from 'components/atoms-temp/multimedia/Image/Image';
+
+// misc
+
 // molecules
 export { Form } from 'components/molecules/Form/Form';
 export { SearchBar } from 'components/molecules/SearchBar/SearchBar';
@@ -26,12 +44,22 @@ export { HeaderTexts } from 'components/molecules/HeaderTexts/HeaderTexts';
 export { FooterButtons } from 'components/molecules/FooterButtons/FooterButtons';
 export { SamplePostList } from 'components/molecules/SamplePostList/SamplePostList';
 
+export { PostHeader } from 'components/molecules/PostHeader/PostHeader';
+export { PostBody } from 'components/molecules/PostBody/PostBody';
+export { Og } from 'components/molecules/Og/Og';
+export { PostFooter } from 'components/molecules/PostFooter/PostFooter';
+export { CommentForm } from 'components/molecules/CommentForm/CommentForm';
+export { CommentThread } from 'components/molecules/CommentThread/CommentThread';
+
 // organisms
 export { ProfileList } from 'components/organisms/ProfileList/ProfileList';
 export { SearchResultTab } from 'components/organisms/SearchResultTab/SearchResultTab';
 export { SearchArea } from 'components/organisms/SearchArea/SearchArea';
 export { Header } from 'components/organisms/Header/Header';
 export { Footer } from 'components/organisms/Footer/Footer';
+
+export { Post } from 'components/organisms/Post/Post';
+export { Comments } from 'components/organisms/Comments/Comments';
 
 // templates
 export { HomePage } from 'components/templates/HomePage/HomePage';
