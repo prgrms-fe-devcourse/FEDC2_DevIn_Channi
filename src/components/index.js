3309b67f3e5b05d22ac484d981f64ae0040480ac
lexie-kaia <kaia.gayeon.kim@gmail.com>
2022-06-17 17:56:23 +0900

    feat: components index.js 수정

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+// atoms
 export { Icon } from 'components/atoms/Icon/Icon';
 export { ProfileUserImage } from 'components/atoms/ProfileUserImage/ProfileUserImage';
 export { ProfileUserName } from 'components/atoms/ProfileUserName/ProfileUserName';
@@ -7,11 +8,37 @@ export { FormBtn } from 'components/atoms/FormBtn/FromBtn';
 export { FormInput } from 'components/atoms/FormInput/FormInput';
 export { FormLink } from 'components/atoms/FormLink/FormLink';
 
+// interactives
+export { WrapperLink } from 'components/atoms-temp/Interactives/WrapperLink/WrapperLink';
+export { TextLink } from 'components/atoms-temp/Interactives/TextLink/TextLink';
+export { IconBtn } from 'components/atoms-temp/Interactives/IconBtn/IconBtn';
+export { TextBtn } from 'components/atoms-temp/Interactives/TextBtn/TextBtn';
+export { RoundInput } from 'components/atoms-temp/Interactives/RoundInput/RoundInput';
+
+// text
+export { Span } from 'components/atoms-temp/Text/Span/Span';
+export { Paragraph } from 'components/atoms-temp/Text/Paragraph/Paragraph';
+export { Time } from 'components/atoms-temp/Text/Time/Time';
+
+// multimedia
+export { Avatar } from 'components/atoms-temp/multimedia/Avatar/Avatar';
+export { Image } from 'components/atoms-temp/multimedia/Image/Image';
+
+// misc
+
 // molecules
 export { Form } from 'components/molecules/Form/Form';
 export { Profile } from 'components/molecules/Profile/Profile';
 
+export { PostHeader } from 'components/molecules/PostHeader/PostHeader';
+export { PostBody } from 'components/molecules/PostBody/PostBody';
+export { Og } from 'components/molecules/Og/Og';
+export { PostFooter } from 'components/molecules/PostFooter/PostFooter';
+export { CommentForm } from 'components/molecules/CommentForm/CommentForm';
+export { CommentThread } from 'components/molecules/CommentThread/CommentThread';
+
+// organisms
 export { ProfileList } from 'components/organisms/ProfileList/ProfileList';
 
-
-
+export { Post } from 'components/organisms/Post/Post';
+export { Comments } from 'components/organisms/Comments/Comments';
