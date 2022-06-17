6fa5ae1c71e8106ff4949d88170d39cf13968d94
parkinhwa <inhwa6548@naver.com>
2022-06-17 12:35:09 +0900

    refactory: search reduxslice 합침

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import allUsersReducer from './allUsers'
-import searchUsersReducer from './searchUsers';
-import searchPostsReducer from './searchPosts'
+import searchReducer from './searchSlice';
 
 export const store = configureStore({
   reducer: {
-    allUsers: allUsersReducer,
-    searchUsers: searchUsersReducer,
-    searchPosts: searchPostsReducer,
+    search: searchReducer,
   },
 });
