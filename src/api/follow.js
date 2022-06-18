50a967957d5815a47f4c9c8748b2458adfddafb4
parkinhwa <inhwa6548@naver.com>
2022-06-18 16:11:43 +0900

    feat: follow API 추가

---
+++ src/api/follow.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API = process.env.REACT_APP_API_BASEURL;
+
+export const follow = {
+  follow: async ({ token, userId }) => {
+    console.log(token, userId);
+    try {
+      const response = await axios.post(
+        `${API}/follow/create`,
+        { userId },
+        { headers: { Authorization: `Bearer ${token}` } },
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  unfollow: async ({ token, id }) => {
+    console.log('Usertoken=', token, "\n",'삭제할팔로잉Id=', id);
+    try {
+      const response = await axios.delete(
+        `${API}/follow/delete`,
+        { id },
+        { headers: { Authorization: `Bearer ${token}` } },
+      );
+      console.log(response.data);
+      return response.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
