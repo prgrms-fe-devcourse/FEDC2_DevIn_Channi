524148589dd79f3b3bfd4531a69372c21cd971c8
parkinhwa <inhwa6548@naver.com>
2022-06-21 14:38:42 +0900

    style: followApi 주석 제거

--- src/api/follow.js
+++ src/api/follow.js
@@ -4,21 +4,18 @@ const API = process.env.REACT_APP_API_BASEURL;
 
 export const follow = {
   follow: async ({ token, userId }) => {
-    console.log(token, userId);
     try {
       const response = await axios.post(
         `${API}/follow/create`,
         { userId },
         { headers: { Authorization: `Bearer ${token}` } },
       );
-      console.log(response.data);
       return response.data;
     } catch (e) {
       console.error(e);
     }
   },
   unfollow: async ({ token, id }) => {
-    console.log('Usertoken=', token, typeof token, '\n', '삭제할팔로잉Id=', id);
     if (token && id) {
       try {
         const response = await axios.delete(`${API}/follow/delete`, {
@@ -27,7 +24,6 @@ export const follow = {
             id,
           },
         });
-        console.log(response.data);
         return response.data;
       } catch (e) {
         console.error(e);
